2103fd53831e99f88b0c83e8b42e206bc47c812c
Pranav Undre <undrepranav@gmail.com>
2023-05-30 21:30:12 +0530

    Leetcode House Robber II

--- 06-House-Robber-II.cpp
+++ 06-House-Robber-II.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int robHouse(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        int prev12 = 0, prev1 = nums[0], prev22 = 0, prev2 = nums[1];
-        for(int i = 1; i < n-1; i++){
-            int take1 = nums[i] + prev12;
-            int notTake1 = prev1;
-            int maxSum1 = max(take1, notTake1);
-            prev12 = prev1;
-            prev1 = maxSum1;
-
-            int take2 = nums[i+1] + prev22;
-            int notTake2 = prev2;
-            int maxSum2 = max(take2, notTake2);
-            prev22 = prev2;
-            prev2 = maxSum2;
+        int prev2 = 0, prev = nums[0];
+        for(int i = 1; i < n; i++){
+            int take = nums[i] + prev2;
+            int notTake = prev;
+            int maxSum = max(take, notTake);
+            prev2 = prev;
+            prev = maxSum;
         }
-        return max(prev1, prev2);
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        vector<int> nums1(nums.begin(), nums.end()-1);
+        vector<int> nums2(nums.begin()+1, nums.end());
+        return nums.size() == 1 ? nums[0] : max(robHouse(nums1), robHouse(nums2));
     }
 };
